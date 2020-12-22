3eda44c6643092c9e0d8cc3575eea712faecf283
abs <abs@ggl.pt>
2020-12-22 22:37:45 +0000

    firebase access

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,10 @@
 const firebaseConfig = {
-	apiKey: "AIzaSyBCTodxv_Mogxv0rJrweAjIJdvvehMv87M",
-	authDomain: "fbui-n.firebaseapp.com",
-	projectId: "fbui-n",
-	storageBucket: "fbui-n.appspot.com",
-	messagingSenderId: "1062557295622",
-	appId: "1:1062557295622:web:36cf37ef64dc60f63f8f87",
-	measurementId: "G-02CGNWR1MP"
+	apiKey: "AIzaSyBLno126jESgr7JzuOADmImv1D0EkBkfNI",
+	authDomain: "mne-pmo.firebaseapp.com",
+	databaseURL: "https://mne-pmo-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "mne-pmo",
+	storageBucket: "mne-pmo.appspot.com",
+	messagingSenderId: "1027876211335",
+	appId: "1:1027876211335:web:98ee15cb97dc0298134622",
+	measurementId: "G-7X7SYD20HZ"
 };
