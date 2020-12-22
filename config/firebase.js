38caa51f8dad91a85503e6332b091eaf38af346b
abs <abs@ggl.pt>
2020-12-22 09:33:41 +0000

    baseline

---
+++ config/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyBCTodxv_Mogxv0rJrweAjIJdvvehMv87M",
+	authDomain: "fbui-n.firebaseapp.com",
+	projectId: "fbui-n",
+	storageBucket: "fbui-n.appspot.com",
+	messagingSenderId: "1062557295622",
+	appId: "1:1062557295622:web:36cf37ef64dc60f63f8f87",
+	measurementId: "G-02CGNWR1MP"
+};
