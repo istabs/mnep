923a6ce63812287104f8672a9b9c6cb2f74a849b
abs <abs@ggl.pt>
2020-12-25 23:31:41 +0000

    view

--- model/data_airtables.js
+++ model/data_airtables.js
@@ -4,15 +4,15 @@ function prepareAirtables(project, chartPlaceholder, rawData) {
 	rawData.sort((a, b) => Date.parse(a.fields.Inicio[0]) - Date.parse(b.fields.Inicio[0])).forEach(item => {
 		if (item.fields["Sumário"]) {
 			var preds = item.fields[project.parent] ? item.fields[project.parent][0] : null;
-			var fim = new Date(item.fields[project.end])
-			fim.setDate(fim.getDate() + 1)
-			if (item.fields.Inicio && item.fields.Fim) {
+			var actvEnd = new Date(item.fields[project.end])
+			actvEnd.setDate(actvEnd.getDate() + 1)
+			if (item.fields[project.start] && item.fields[project.end]) {
 				rows.push([
 					item.id, // Task ID
 					item.fields[project.label], // Task Name
 					item.fields[project.group], // Group (string)
 					new Date(item.fields[project.start]), // Start Date
-					fim, // End Date
+					actvEnd, // End Date
 					0, // Duration (number)
 					0, // Percent Complete (number)
 					preds, // Dependencies (string / comma separated)
