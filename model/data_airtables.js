ea897603943f07bd421259321ee0babbf5309320
abs <abs@ggl.pt>
2020-12-25 23:35:21 +0000

    view

--- model/data_airtables.js
+++ model/data_airtables.js
@@ -26,7 +26,7 @@ function prepareAirtables(project, chartPlaceholder, rawData) {
 }
 
 function airtables1(project, chartPlaceholder) {
-	document.getElementById(chartPlaceholder).innerHTML = project.name;
+	document.getElementById('chart_title').textContent = project.name;
 	let url = "https://api.airtable.com/v0/" + project.key + "/" + project.table;
 	readAirtablesData(url, project, chartPlaceholder, rawData, prepareAirtables);
 }
