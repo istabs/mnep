cbd79283709e9676caa611e80395e72af8e9b07f
abs <abs@ggl.pt>
2020-12-25 16:05:34 +0000

    firebase access

--- model/data_airtables.js
+++ model/data_airtables.js
@@ -1,12 +1,13 @@
 var rawData = [];
 
+function prepareAirtables(project, chartPlaceholder, rawData) {
+	console.log(project);
+	console.log(chartPlaceholder);
+	console.log(rawData);
+}
+
 function airtables1(project, chartPlaceholder) {
 	document.getElementById(chartPlaceholder).innerHTML = project.name;
 	let url = "https://api.airtable.com/v0/" + project.key + "/" + project.table;
-	readAirtablesData(url, project, chartPlaceholder, rawData,
-	(project, chartPlaceholder, rawData) => {
-		console.log(project);
-		console.log(chartPlaceholder);
-		console.log(rawData);
-	});
+	readAirtablesData(url, project, chartPlaceholder, rawData, prepareAirtables);
 }
