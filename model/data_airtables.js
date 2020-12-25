e1654a4169d3afa615a32fd400ed44eeafbdfb58
abs <abs@ggl.pt>
2020-12-25 16:15:03 +0000

    firebase access

--- model/data_airtables.js
+++ model/data_airtables.js
@@ -1,9 +1,27 @@
-var rawData = [];
 
 function prepareAirtables(project, chartPlaceholder, rawData) {
-	console.log(project);
-	console.log(chartPlaceholder);
+	var rows = [];
 	console.log(rawData);
+	rawData.sort((a, b) => Date.parse(a.fields.Inicio[0]) - Date.parse(b.fields.Inicio[0])).forEach(item => {
+		if (item.fields["Sumário"]) {
+			var preds = item.fields.Predecessores ? item.fields.Predecessores[0] : null;
+			var fim = new Date(item.fields["Fim"])
+			fim.setDate(fim.getDate() + 1)
+			if (item.fields.Inicio && item.fields.Fim) {
+				rows.push([
+					item.id, // Task ID
+					item.fields["Atividade"], // Task Name
+					item.fields["Classificação"], // Group (string)
+					new Date(item.fields["Inicio"]), // Start Date
+					fim, // End Date
+					0, // Duration (number)
+					0, // Percent Complete (number)
+					preds, // Dependencies (string / comma separated)
+				]);
+			}
+		}
+	});
+	console.log(rows);
 }
 
 function airtables1(project, chartPlaceholder) {
