fdf5b4a91021ac784365a9e12858e5d06506be57
abs <abs@ggl.pt>
2020-12-26 22:38:29 +0000

    details implementation

--- view/gantt_placeholder.js
+++ view/gantt_placeholder.js
@@ -49,8 +49,7 @@ function presentGantt(chartPlaceholder,
 function prepareAirtables2(project, chartPlaceholder, rawData, rule) {
 	var rows = [];
 	rawData.sort((a, b) => Date.parse(a.fields.Inicio[0]) - Date.parse(b.fields.Inicio[0])).forEach(item => {
-		console.log('---')
-		if (item['Predecessores']) console.log(item['Predecessores'])
+		console.log(item)
 		//console.log(Array.isArray(item['Predecessores']) ? item['Predecessores'][0] : item['Predecessores'])
 		if (rule(item['id']) || rule(Array.isArray(item['Predecessores']) ? item['Predecessores'][0] : item['Predecessores'])) {
 			let actvEnd = new Date(item.fields[project.end])
