6a35df06d5a52262935bb74011a2dbf2f198649d
abs <abs@ggl.pt>
2020-12-27 18:06:14 +0000

    details implementation

--- view/presenter.js
+++ view/presenter.js
@@ -76,6 +76,35 @@ function prepareAirtables(project, chartPlaceholder, rawData) {
 function prepareAirtables2(project, chartPlaceholder, rawData, rule) {
 	var rows = [];
 	rawData.sort((a, b) => Date.parse(a.fields.Inicio[0]) - Date.parse(b.fields.Inicio[0])).forEach(item => {
+		console.log(Array.isArray(item.fields['Predecessores']) ? item.fields['Predecessores'][0] : item.fields['Predecessores'])
+		//if (rule(item['id']) || rule(Array.isArray(item.fields['Predecessores']) ? item.fields['Predecessores'][0] : item.fields['Predecessores'])) {
+		if (rule(item)) {
+			let actvEnd = new Date(item.fields[project.end])
+			let progress = project.progress ? (item.fields[project.progress] ? item.fields[project.progress] : 0) : 0;
+			let preds = project.parent ? (item.fields[project.parent] ? item.fields[project.parent][0] : null) : null;
+			actvEnd.setDate(actvEnd.getDate() + 1)
+			if (item.fields[project.start] && item.fields[project.end]) {
+				rows.push([
+					item.id, // Task ID
+					item.fields[project.label], // Task Name
+					item.fields[project.group], // Group (string)
+					new Date(item.fields[project.start]), // Start Date
+					actvEnd, // End Date
+					0, // Duration (number)
+					progress, // Percent Complete (number)
+					preds, // Dependencies (string / comma separated)
+				]);
+			}
+		}
+	});
+	presentGantt(chartPlaceholder,
+		{ gantt: { criticalPathEnabled: true, criticalPathStyle: { stroke: '#e64a19', }, arrow: { radius: 10 } },
+		height: project.height * 42 + 40, width: 960 }, rawData, rows, project);
+}
+
+function prepareAirtables3(project, chartPlaceholder, rawData, rule) {
+	var rows = [];
+	rawData.sort((a, b) => Date.parse(a.fields.Inicio[0]) - Date.parse(b.fields.Inicio[0])).forEach(item => {
 		//console.log(Array.isArray(item.fields['Predecessores']) ? item.fields['Predecessores'][0] : item.fields['Predecessores'])
 		//if (rule(item['id']) || rule(Array.isArray(item.fields['Predecessores']) ? item.fields['Predecessores'][0] : item.fields['Predecessores'])) {
 		if (rule(item)) {
